b352765d8b153c2c1fa7d4027262baff87a47a9c
Joel Lee <joel@Joels-MacBook-Pro.local>
2021-01-04 15:14:31 +0800

    Update schema of posts

--- lib/chirp/timeline/post.ex
+++ lib/chirp/timeline/post.ex
@@ -3,10 +3,10 @@ defmodule Chirp.Timeline.Post do
   import Ecto.Changeset
 
   schema "posts" do
-    field :body, :string
-    field :likes_count, :integer
-    field :reposts_count, :integer
-    field :username, :string
+    field(:body, :string)
+    field(:likes_count, :integer, default: 0)
+    field(:reposts_count, :integer, default: 0)
+    field(:username, :string, default: "chirpy")
 
     timestamps()
   end
@@ -14,7 +14,8 @@ defmodule Chirp.Timeline.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:username, :body, :likes_count, :reposts_count])
-    |> validate_required([:username, :body, :likes_count, :reposts_count])
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+    |> validate_length(:body, min: 2, max: 250)
   end
 end
